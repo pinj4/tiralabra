627b61a804bc48fc6ec7ab5a956b2ce1d1cc84b7
Pinja <pinja.salomaki@gmail.com>
2023-06-22 15:08:37 +0300

    koodin muokkausta

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -14,14 +14,9 @@ class Dijkstra:
         self.goal_node = goal_node
         self.prev = [-1] * (self.n*self.n)
     
-        #self.path = []
-        #self.parents = [-1] * self.n*self.n
-    
-
     def dijkstra(self):
         distances = [inf] * (self.n * self.n)  
         distances[self.start_node] = 0
-        #self.parents[self.start_node] = "NO PARENTS"
         heap = []
         heappush(heap, (0, self.start_node))
         done = []
@@ -31,7 +26,6 @@ class Dijkstra:
             if node in done:
                 continue
             done.append(node)
-            #self.path.append(node)
             for child in self.g[node]:
                 current = distances[child[0]]
                 new = distances[node] + child[1]
@@ -39,24 +33,9 @@ class Dijkstra:
                     distances[child[0]] = distances[node] + child[1]
                     heappush(heap, (distances[child[0]], child[0]))
                     self.prev[child[0]] = node
-                    #self.parents[child[0]] = node
-                    #self.path.append(child[0])
 
-        #self.print_solution(distances)
         return distances[self.goal_node]
-    
-    #def print_solution(self, distances):
-        #print(distances[self.goal_node])
-        #print(self.parents)
-       # for node in range(len(distances)):
-            #self.print_path(node)
-    
-    #def print_path(self, node):
-        #if node == "NO PARENTS":
-            #return
-        #self.print_path(self.parents[node])
-        #print(node, end= " ")
-    
+
     def get_path(self):
         S = []
         u = self.goal_node
@@ -66,4 +45,3 @@ class Dijkstra:
         S.append(self.start_node)
         S.reverse()
         return S
-
